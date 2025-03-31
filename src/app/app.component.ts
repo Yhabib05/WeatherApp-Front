0f5fb30503f59c7c36f9301ed5cbe8ba2a85d5c7
Yassin Habib <yassin.habib@grenoble-inp.org>
2025-03-31 23:30:24 +0200

    starting angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'weatherAppFrontEnd';
+  username:string = '';
+  city:string = 'Paris';
+  favoriteCity: string | null = null;
+
+  saveUser(){
+    alert(`Hello ${this.username}`);
+  }
+
+  toggleFavorite(){
+    console.log("toggleFavorite has been called");
+    if(this.favoriteCity===this.city){
+      this.favoriteCity = null;
+    } else{
+      this.favoriteCity = this.city;
+    }
+  }
+
+  isFavorite():boolean{
+    console.log("isFavorite called", this.favoriteCity === this.city);
+    return this.favoriteCity === this.city;
+  }
+
 }
+
