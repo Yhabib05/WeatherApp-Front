7c56ff2ad6fc7e44036906f67603b36d9e1e8dc0
Yassin Habib <yassin.habib@grenoble-inp.org>
2025-04-11 13:46:31 +0200

    mvp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,9 @@ interface Coord {
 
 interface City {
   name: string;
-  cityCoord: Coord;
+  cityCoord: Coord | null;
+  weatherInfos: any | null;
+  temperature?: number; // optional, set after fetching the weather
 }
 
 interface User {
@@ -28,138 +30,190 @@ interface User {
 })
 
 export class AppComponent {
-  username:string = '';
-  city1:string = 'paris';
-  city2:string = 'toulouse';
-  favoriteCity: string = '';
-  userId: string = '';
-  weatherInfo: any = null;
-  cityCoords: { [city: string]: Coord } = {};
-  savedCities: string[] = [];
-
-
-constructor(private http: HttpClient) {}
-
+  user: User | null = null;
+  username: string = '';
+  newCityName: string = '';
+  createdCities: City[] = [];
   isEditingUser: boolean = false;
 
+  constructor(private http: HttpClient) {}
 
-  saveUser() {
-    const payload = {username: this.username};
-    this.http.post<User>('http://localhost:8080/user', payload).subscribe({
+  saveUser(): void {
+    const payload = {username: this.username.trim() };
+    if(!payload.username){
+      alert('Username cannot be empty!');
+      return;
+    }
+    this.http.post<User>('http://localhost:8080/users', payload).subscribe({
       next: (res) => {
-        this.userId = res.id;
+        this.user = res;
+        this.username = res.username;
         this.isEditingUser = false;
-        alert(`User ${this.username} created!`);
+        alert(`User ${this.user.username} created!`);
       },
-      error: () => alert('Error creating user'),
+      error: (e) => alert('Error creating user' + e),
     });
   }
+
   setEditUser() {
     this.isEditingUser = true;
   }
 
-  changeName(newName: string) {
-    const payload = {id: this.userId};
-    this.http.put<User>('http://localhost:8080/user', newName,{
-      params: { id:this.userId }
+  changeName(newName: string): void {
+    if (!this.user || !newName.trim()) return;
+
+    this.http.put<User>('http://localhost:8080/users', newName.trim(),{
+      params: { id:this.user.id },
     }).subscribe({
       next: (res) => {
-        this.userId = res.id;
+        this.user= res;
         this.username = res.username;
         this.isEditingUser = false;
-        alert(`User name ${this.username} changed!`);
+        alert(`Username changed to ${res.username}`);
         },
       error: () => alert('Failed to change user name')
     })
   }
 
-  newCityName: string = '';
-  createdCities: string[] = [];
-
-  addCity() {
-    const city = this.newCityName.trim().toLowerCase();
-    if (!city || this.createdCities.includes(city)) {
+  addCity(): void {
+    const name = this.newCityName.trim().toLowerCase();
+    if (!name || this.createdCities.some(c => c.name === name)) {
       alert('City name is empty or already exists');
       return;
     }
 
+    const city = {
+      name,
+      cityCoord:null,
+      weatherInfos:null
+    };
     this.createdCities.push(city);
     this.newCityName = '';
+    this.getCoordFromCity(city);
   }
 
-  saveCity(city: string) {
-    if (!this.savedCities.includes(city)) {
-      this.savedCities.push(city);
-      alert(`City "${city}" saved locally.`);
-    } else {
-      alert(`City "${city}" is already saved.`);
-    }
-  }
 
-  getWeatherByCity(city: string) {
-    this.http.get<any>('http://localhost:8080/weather/by-city', {
-      params: { city }
-    }).subscribe({
+  getCoordFromCity(city: City): void {
+    const cityFound = this.createdCities.find(c => c.name === city.name);
+    if(!cityFound || cityFound.cityCoord) return;
+    this.http.get<Coord>(`http://localhost:8080/coord/${cityFound.name}`).subscribe({
       next: (res) => {
-        this.weatherInfo = res;
-        console.log("Weather info for", city, res);
+        cityFound.cityCoord = {lat: res.lat, lon: res.lon};
+        //get the temperature the first time when adding the city
+        this.getTemperatureByCoord(cityFound);
 
-        const coord: Coord = {
-          lat: res.coord.lat,
-          lon: res.coord.lon,
-        }
-        this.cityCoords[city] = coord;
-
-     },
+        console.log(`the coordinates for the city ${cityFound.name} are: `, cityFound.cityCoord);
+      },
       error: () => {
-        alert(`Could not fetch weather for ${city}`);
+        alert(`Could not get coordinates for ${cityFound.name}`);
+      }
+    });
+  }
+
+  getWeatherByCoord(city: City): void {
+    const cityFound = this.createdCities.find(c => c.name === city.name);
+    if(!cityFound) return;
+    const url = `http://localhost:8080/weather`;
+    this.http.post<any>(url,cityFound.cityCoord).subscribe({
+      next: (res) => {
+        cityFound.weatherInfos = res
+        cityFound.temperature = res.main?.temp-273.15;
+      },
+      error: () => {
+        alert(`Could not fetch weather for ${cityFound.name}`);
+      }
+    });
+  }
+
+  getTemperatureByCoord(city: City): void {
+    const cityFound = this.createdCities.find(c => c.name === city.name);
+    if(!cityFound) return;
+    const url = `http://localhost:8080/weather/temperature`;
+    this.http.post<any>(url,cityFound.cityCoord).subscribe({
+      next: (res) => {
+        cityFound.temperature = res-273.15;
+        console.log("Weather info for", cityFound.name, res);
+      },
+      error: () => {
+        alert(`Could not fetch temperature for ${cityFound.name}`);
       }
     });
   }
 
 
-  toggleFavorite(city: string ) {
-    if (!this.username || !this.userId) {
+  /* to remove */
+  getWeatherByCity(cityName: string): void {
+    this.http.get<any>('http://localhost:8080/weather/by-city', {
+      params: { city: cityName }
+    }).subscribe({
+      next: (res) => {
+        const city = this.createdCities.find(c => c.name === cityName);
+
+        if(!city) return;
+
+        city.cityCoord = {lat: res.coord.lat, lon: res.coord.lon};
+        city.temperature = res.main?.temp-273.15;
+
+        console.log("Weather info for", cityName, res);
+     },
+      error: () => {
+        alert(`Could not fetch weather for ${cityName}`);
+      }
+    });
+  }
+
+
+  toggleFavorite(cityName: string ) {
+    if (!this.user) {
       alert('Save user first');
       return;
     }
 
-    const coord = this.cityCoords[city];
-
-    if (!coord) {
-      alert('Cant save it to favorites, city wasnt correctly saved' );
+    const city = this.createdCities.find(c => c.name === cityName);
+    if (!city || !city.cityCoord) {
+      alert('City coord not found' );
       return;
     }
 
-    const isSame = this.favoriteCity===city;
-    console.log(isSame);
+    const isFavorite = this.isFavorite(cityName);
 
-    if (isSame) {
+    const url = `http://localhost:8080/users/${this.user.id}/favorite`;
+    const options = {
+      body: city.cityCoord,
+      params:{ userId: this.user.id },
+      responseType: 'text' as const
+    }
+
+    if (isFavorite) {
       // Remove from favorite
       this.http
-        .delete('http://localhost:8080/user/favorite', {
-          body: coord,
-          params: {userId: this.userId},
-          responseType: "text" as const //bcs the backend returns a text: city removed from  favorite
-        })
-        .subscribe(() => {
-          this.favoriteCity = '';
+        .delete(url,options)
+        .subscribe({
+        next: () => {
+          this.user!.favoriteCity = null;
+          alert(`${cityName} removed from favorites`);
+        },
+          error: () =>alert('Failed to remove favorite city')
         });
     } else {
       // Add to favorite
       this.http
-        .post(`http://localhost:8080/user/favorite`,city,{
-            params:{userId: this.userId},
+        .post(url,city.cityCoord,{
+            params:{userId: this.user.id},
           responseType: "text" as const //bcs the backend returns a text: city added to favorite
         })
-        .subscribe(() => {
-          this.favoriteCity = city;
+        .subscribe({
+          next: ()  =>{
+            this.user!.favoriteCity = city;
+            alert(`${cityName} added to favorites`);
+          },
+          error: ()=> alert('Error adding to favorites')
         });
     }
   }
 
-  isFavorite(city: string): boolean {
-    return  this.favoriteCity==city;
+  isFavorite(cityName: string): boolean {
+    return  this.user?.favoriteCity?.name===cityName;
   }
 }
 
